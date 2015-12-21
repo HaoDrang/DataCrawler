693c37506ee8b58d554d77ca63f191e58517a7b7
sherlockchang <sherlockchang@outlook.com>
2015-12-21 16:55:16 +0800

    update ignore and add a new java projec

---
+++ SmallCrawler/src/CrawlerBody.java
@@ -0,0 +1,15 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CrawlerBody {
+
+	public static void main(String[] args) {
+		
+		Logger log = Logger.getLogger(CrawlerBody.class.getName());
+		
+		log.log(Level.SEVERE, "LogTest");
+		
+		
+	}
+
+}
